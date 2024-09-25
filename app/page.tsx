4b2fbcb64e9d6484bf6fad5e03dc72b71a347a66
e225724 <e225724@eve.u-ryukyu.ac.jp>
2024-09-25 18:51:38 +0900

    nextjs

---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect } from "react";
+import Head from "next/head";
+import mapboxgl from "mapbox-gl"; // mapbox-glのインポート
+
+export default function Home() {
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN) {
+      throw new Error("NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN is not set");
+    }
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+    // マップを初期化
+    const map = new mapboxgl.Map({
+      container: "map", // マップを表示するdivのID
+      style: "mapbox://styles/mapbox/streets-v11", // マップスタイル
+      center: [139.7670516, 35.6811673], // 地図の中心座標（東京駅）
+      zoom: 15, // ズームレベル
+    });
+
+    console.log(map); // mapオブジェクトの確認
+
+    // コンポーネントがアンマウントされたときにマップを削除
+    return () => map.remove();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>地図表示</title>
+        <meta charSet="utf-8" />
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <div
+        id="map"
+        style={{
+          height: "100vh",
+          width: "100%",
+        }}
+      ></div>
+    </>
+  );
+}
